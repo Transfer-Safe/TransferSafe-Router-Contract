d926f175e7b5e35c68b5b0fd571a8f417162c543
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-07-17 21:51:27 +0300

    test script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config'
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const { API_URL, PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "polygon_mumbai",
+   networks: {
+      hardhat: {},
+      polygon_mumbai: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
 };
 
 export default config;
